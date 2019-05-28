fe11f443996cfb41ffdbceb1b0bda5b540d6070a
Sadman Hossain <sakibsadman2525@gmail.com>
2019-05-29 02:16:51 +0600

    Updates

---
+++ precache-manifest.0e401ea63a6b5f4d23863d54dda4db5e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280fb9eefd96acd416c5c4580412bd32",
+    "url": "/Shadman210/devSadman.git/index.html"
+  },
+  {
+    "revision": "934a86968032893ddf62",
+    "url": "/Shadman210/devSadman.git/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "d865664fc6809d0c2e59",
+    "url": "/Shadman210/devSadman.git/static/css/main.ce7322e2.chunk.css"
+  },
+  {
+    "revision": "934a86968032893ddf62",
+    "url": "/Shadman210/devSadman.git/static/js/2.8edfc993.chunk.js"
+  },
+  {
+    "revision": "d865664fc6809d0c2e59",
+    "url": "/Shadman210/devSadman.git/static/js/main.7a27ad49.chunk.js"
+  },
+  {
+    "revision": "38aec8a2873ffd1470da",
+    "url": "/Shadman210/devSadman.git/static/js/runtime~main.b70deb99.js"
+  },
+  {
+    "revision": "3a18dbbab75b7b7a03651cf326255248",
+    "url": "/Shadman210/devSadman.git/static/media/resume.3a18dbba.jpg"
+  },
+  {
+    "revision": "08bfffdd6003388fb11f4f4184fc034e",
+    "url": "/Shadman210/devSadman.git/static/media/sadman.08bfffdd.jpg"
+  }
+]);
