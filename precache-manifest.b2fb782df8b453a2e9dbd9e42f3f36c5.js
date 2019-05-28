5434a479c8c9e18eddd10dbd19974fe592119799
Sadman Hossain <sakibsadman2525@gmail.com>
2019-05-29 02:54:52 +0600

    Updates

---
+++ precache-manifest.b2fb782df8b453a2e9dbd9e42f3f36c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c080183e123e69875825889c5f7da806",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c01e7952cf683c55c398",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "c568141123fe871d4323",
+    "url": "/static/css/main.ce7322e2.chunk.css"
+  },
+  {
+    "revision": "c01e7952cf683c55c398",
+    "url": "/static/js/2.14392966.chunk.js"
+  },
+  {
+    "revision": "c568141123fe871d4323",
+    "url": "/static/js/main.c88d99ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3a18dbbab75b7b7a03651cf326255248",
+    "url": "/static/media/resume.3a18dbba.jpg"
+  },
+  {
+    "revision": "08bfffdd6003388fb11f4f4184fc034e",
+    "url": "/static/media/sadman.08bfffdd.jpg"
+  }
+]);
