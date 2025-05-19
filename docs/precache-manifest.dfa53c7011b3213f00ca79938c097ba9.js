b6598d893c9ab851f512af74b5ee632836c5f753
neutrix-developer-sixteen <developer.sixteen@neutrix.co>
2025-05-20 03:13:17 +0600

    Add build output to docs for GitHub Pages

---
+++ docs/precache-manifest.dfa53c7011b3213f00ca79938c097ba9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee45cd26b6f5d9131b96ba9b4cc7ba61",
+    "url": "/devSadman.com/index.html"
+  },
+  {
+    "revision": "51f7fc43e751a50890d9",
+    "url": "/devSadman.com/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "f6e8b585284f13bed67e",
+    "url": "/devSadman.com/static/css/main.0543989c.chunk.css"
+  },
+  {
+    "revision": "51f7fc43e751a50890d9",
+    "url": "/devSadman.com/static/js/2.ca9a4554.chunk.js"
+  },
+  {
+    "revision": "f6e8b585284f13bed67e",
+    "url": "/devSadman.com/static/js/main.5e0afb34.chunk.js"
+  },
+  {
+    "revision": "5cf7d0e0a2ace9ebeba3",
+    "url": "/devSadman.com/static/js/runtime~main.8d79ffd9.js"
+  },
+  {
+    "revision": "3a18dbbab75b7b7a03651cf326255248",
+    "url": "/devSadman.com/static/media/resume.3a18dbba.jpg"
+  },
+  {
+    "revision": "08bfffdd6003388fb11f4f4184fc034e",
+    "url": "/devSadman.com/static/media/sadman.08bfffdd.jpg"
+  }
+]);
